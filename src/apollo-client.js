61a69313cc344b719a076d753091bd1f626fb2bd
Saad Amir <samir@apac.microagility.com>
2025-08-20 19:47:16 +0500

    feat(websocket): configure Apollo Client with WebSocket subscription support

---
+++ src/apollo-client.js
@@ -0,0 +1,53 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+  split,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { getMainDefinition } from "@apollo/client/utilities";
+
+// HTTP link for queries and mutations
+const httpLink = createHttpLink({
+  uri: "http://localhost:3000/graphql",
+});
+
+// WebSocket link for subscriptions
+const wsLink = new WebSocketLink({
+  uri: 'ws://localhost:3000/graphql',
+  options: {
+    reconnect: true,
+  },
+});
+
+// Auth link to add JWT token to requests
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("access_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+// Split link - use WebSocket for subscriptions, HTTP for queries/mutations
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+
+const client = new ApolloClient({
+  link: splitLink,
+  cache: new InMemoryCache(),
+});
+
+export default client;
